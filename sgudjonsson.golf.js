5a4e88e3bceb64711b08bde6a037375306739764
Sigurbjorn Gudjonsson <sigurbjorn.gudjonsson@gmail.com>
2012-06-02 00:59:51 +0000

    Added new handicap calculations
    Needs refactoring

--- sgudjonsson.golf.js
+++ sgudjonsson.golf.js
@@ -37,6 +37,81 @@ sgudjonsson.golf = (function() {
 			}
 			return stablefordPoints;
 		},
+		_getNewHandicap: function(playerCurrentHandicap, stablefordPoints, is18) {
+
+			var categories = [
+				{
+					Category: 1,
+					HcpMax: 4.4,
+					Inc: 0.1,
+					Dec: 0.1,
+					BufferFrom18: 35,
+					Buffer9: undefined,
+				},
+				{
+					Category: 2,
+					HcpMax: 11.4,
+					Inc: 0.1,
+					Dec: 0.2,
+					BufferFrom18: 34,
+					Buffer9: undefined,
+				},
+				{
+					Category: 3,
+					HcpMax: 18.4,
+					Inc: 0.1,
+					Dec: 0.3,
+					BufferFrom18: 33,
+					Buffer9: 35,
+				},
+				{
+					Category: 4,
+					HcpMax: 26.4,
+					Inc: 0.1,
+					Dec: 0.4,
+					BufferFrom18: 32,
+					Buffer9: 34,
+				},
+				{
+					Category: 5,
+					HcpMax: 36,
+					Inc: 0.2,
+					Dec: 0.5,
+					BufferFrom18: 31,
+					Buffer9: 33,
+				}
+			];
+
+			var playerCategory = categories[categories.length - 1];
+			for(var i = categories.length - 1; i >= 0; i--)
+				if(categories[i].HcpMax >= playerCurrentHandicap)
+					playerCategory = categories[i];
+
+			var buffer = is18 ? playerCategory.BufferFrom18 : playerCategory.Buffer9;
+			if(!is18)
+				stablefordPoints += 18;
+
+			if(buffer != undefined) {
+				if(stablefordPoints >= buffer && stablefordPoints <= 36)
+					return parseFloat(parseFloat(playerCurrentHandicap).toFixed(1));
+				if(stablefordPoints < buffer)
+					return parseFloat(parseFloat(playerCurrentHandicap + playerCategory.Inc).toFixed(1));
+			}
+
+			for(var i = 0; i < Math.abs(stablefordPoints - 36); i++) {
+
+				// get the current category
+				var playerCategory = categories[categories.length - 1];
+				for(var c = categories.length - 1; c >= 0; c--)
+					if(categories[c].HcpMax >= playerCurrentHandicap)
+						playerCategory = categories[c];
+
+
+				playerCurrentHandicap -= playerCategory.Dec;
+			}
+
+			return parseFloat(parseFloat(playerCurrentHandicap).toFixed(1));
+		}
 	};
 
 	return {
@@ -48,6 +123,10 @@ sgudjonsson.golf = (function() {
 		getStablefordPoints: function(adjustedPar, strokes) {
 			return _methods._getStablefordPoints(adjustedPar, strokes);
 		},
+
+		getNewHandicap: function(playerCurrentHandicap, stablefordPoints, is18) {
+			return _methods._getNewHandicap(playerCurrentHandicap, stablefordPoints, is18);
+		}
 
 	}
 })();
