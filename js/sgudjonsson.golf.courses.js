478b79d8e3df15a5cc1e1227f8e31cc5f75db460
Sigurbjorn Gudjonsson <sigurbjorn.gudjonsson@gmail.com>
2012-10-11 21:46:26 +0000

    Work in progress

---
+++ js/sgudjonsson.golf.courses.js
@@ -0,0 +1,46 @@
+
+var sgudjonsson = sgudjonsson || {};
+sgudjonsson.golf = sgudjonsson.golf || {};
+
+sgudjonsson.golf.courses = [{
+
+	id: "GHR1"
+	club: "GHR",
+	name: "Strandarvöllur",
+
+	location: {
+		latitude: 63.799374,
+		longitude: -20.299001,
+	},
+
+	courseRating: [
+		{ course: "white",      cr: 69.4, slope: 115, distances: [305,140,536,192,472,347,264,175,332,241,175,301,149,387,527,347,332,350] },
+		{ course: "yellow",     cr: 67.7, slope: 114, distances: [305,140,503,192,435,310,264,144,321,241,175,301,128,387,489,307,290,320] },
+		{ course: "women-blue", cr: 70.7, slope: 119, distances: [270,126,459,153,388,261,242,144,321,206,200,130,264,113,331,489,290,320] },
+		{ course: "women-red",  cr: 68.8, slope: 114, distances: [270,126,459,153,388,261,242,132,294,206,200,130,264,113,331,393,257,237] },
+		{ course: "men-blue",   cr: 65.4, slope: 108, distances: [270,126,459,153,388,261,242,144,321,206,200,130,264,113,331,489,290,320] },
+		{ course: "men-red",    cr: 64.1, slope: 102, distances: [270,126,459,153,388,261,242,132,294,206,200,130,264,113,331,393,257,237] },
+	],
+
+	holes: [
+		{ nr:  1, par: 4, hdcp:  8 },
+		{ nr:  2, par: 3, hdcp: 16 },
+		{ nr:  3, par: 5, hdcp:  2 },
+		{ nr:  4, par: 3, hdcp: 10 },
+		{ nr:  5, par: 5, hdcp:  4 },
+		{ nr:  6, par: 4, hdcp: 14 },
+		{ nr:  7, par: 4, hdcp: 18 },
+		{ nr:  8, par: 3, hdcp: 12 },
+		{ nr:  9, par: 4, hdcp:  6 },
+		{ nr: 10, par: 4, hdcp: 11 },
+		{ nr: 11, par: 3, hdcp: 17 },
+		{ nr: 12, par: 4, hdcp:  3 },
+		{ nr: 13, par: 3, hdcp:  7 },
+		{ nr: 14, par: 4, hdcp:  1 },
+		{ nr: 15, par: 5, hdcp:  5 },
+		{ nr: 16, par: 4, hdcp: 13 },
+		{ nr: 17, par: 4, hdcp: 15 },
+		{ nr: 18, par: 4, hdcp:  9 },
+	]
+
+}];
